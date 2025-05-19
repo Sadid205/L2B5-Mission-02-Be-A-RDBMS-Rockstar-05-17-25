05c921dbd30f62a93633dcaa69d118d9fc89a959
Md.Abdullah al sadid <abdullahalsadid1914@gmail.com>
2025-05-19 23:55:45 +0600

    practice module-9

---
+++ module_9_dates_relationships_join/practice.sql
@@ -0,0 +1,157 @@
+-- Create a foreign key constraint on department_id in the students table referencing departments(id).
+CREATE TABLE students(
+    id  SERIAL PRIMARY KEY,
+    name    VARCHAR(50),
+    department_id INT REFERENCES departments(department_id) ON DELETE CASCADE,
+    last_login  DATE
+);
+
+ALTER TABLE students
+ADD constraint fk_students_departments
+FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL;
+
+
+create TABLE departments(
+    department_id  SERIAL PRIMARY KEY,
+    name    VARCHAR(10)
+);
+
+
+CREATE TABLE courses(
+    id  SERIAL PRIMARY KEY,
+    title VARCHAR(50)
+);
+
+ALTER TABLE departments
+ALTER COLUMN name TYPE VARCHAR(50);
+
+INSERT INTO departments (name) VALUES
+('CS'),
+('Business'),
+('Mech Eng'),
+('Elec Eng'),
+('Civil Eng'),
+('Biotech'),
+('Psychology'),
+('Math'),
+('Physics'),
+('Chemistry');
+
+
+
+INSERT INTO students(name,department_id,last_login) VALUES
+('Arif Rahman', 1, '2025-05-01'),
+('Bithi Akter', 2, '2025-05-02'),
+('Chowdhury Hasan', 3, '2025-05-03'),
+('Dipa Saha', 4, '2025-05-04'),
+('Emran Hossain', 5, '2025-05-05'),
+('Farzana Ahmed', 6, '2025-05-06'),
+('Gazi Mahmud', 7, '2025-05-07'),
+('Hasan Karim', 8, '2025-05-08'),
+('Ishrat Jahan', 9, '2025-05-09'),
+('Jahidul Islam', 10, '2025-05-10');
+
+
+
+
+
+
+INSERT INTO courses(title) VALUES
+('Introduction to Programming'),
+('Calculus I'),
+('Physics for Engineers'),
+('Organic Chemistry'),
+('Genetics'),
+('Shakespearean Literature'),
+('World History'),
+('Microeconomics'),
+('Cognitive Psychology'),
+('Ethics and Philosophy'),
+('International Relations'),
+('Social Research Methods'),
+('Marketing Principles'),
+('Mechanical Engineering Basics'),
+('Human Anatomy'),
+('Constitutional Law'),
+('Music Theory'),
+('Painting Techniques'),
+('Educational Psychology'),
+('Data Analysis');
+
+
+SELECT * FROM students;
+
+-- Retrieve students who have logged in within the last 30 days.
+SELECT * FROM students
+WHERE last_login BETWEEN ( CURRENT_DATE - INTERVAL '30 days') AND CURRENT_DATE;
+
+SELECT min(last_login),max(last_login) FROM students;
+
+-- Extract the login month from the last_login and group students by month.
+SELECT extract(MONTH FROM last_login) as month_number,TO_CHAR(last_login,'Month') as month_name,count(*) as total_students FROM students
+GROUP BY month_number,TO_CHAR(last_login,'Month')
+ORDER BY month_number;
+
+
+-- Count how many students logged in per month and show only those months where login count is more than 3.
+SELECT extract(MONTH FROM last_login) as month_number,TO_CHAR(last_login,'Month') as month_name,count(*) as total_logged_in FROM students
+GROUP BY month_number,month_name
+HAVING count(*) > 3
+ORDER BY month_number;
+
+
+-- Try inserting a student with a department_id that doesn’t exist and observe the behavior.
+INSERT INTO students(name,department_id,last_login) VALUES
+('korim',60,'2025-05-02');
+
+-- Delete a department and see how students are affected using ON DELETE CASCADE and ON DELETE SET NULL.
+DELETE FROM departments
+WHERE id = 5;
+
+SELECT * FROM students;
+DROP TABLE students;
+DROP TABLE departments;
+SELECT * FROM students WHERE department_id = 6;
+
+-- Join students and departments using INNER JOIN to display each student's department name.
+SELECT students.*,departments.name as department_name FROM students
+INNER JOIN departments ON students.department_id=departments.id;
+
+-- Use a LEFT JOIN to show all students including those without a department.
+SELECT students.*,departments.name as department_name FROM students
+LEFT JOIN departments ON students.department_id=departments.id;
+
+-- Use a RIGHT JOIN to show all departments including those without students.
+
+SELECT students.*,departments.name as department_name FROM students
+RIGHT JOIN departments ON students.department_id=departments.id;
+-- Perform a FULL JOIN to get all records from both students and departments.
+SELECT students.*,departments.name as department_name FROM students
+FULL JOIN departments ON students.department_id=departments.id;
+
+-- Create a cross-product of all students and courses using CROSS JOIN.
+SELECT * FROM students
+CROSS JOIN courses;
+
+-- Use NATURAL JOIN between tables that have a shared column like department_id.
+
+
+ALTER TABLE departments
+RENAME COLUMN id to department_id;
+
+
+
+-- Use NATURAL JOIN between tables that have a shared column like department_id.
+
+
+SELECT * FROM students
+JOIN departments USING(department_id);
+
+SELECT * FROM students
+NATURAL JOIN departments;
+
+
+SELECT * FROM students;
+SELECT * FROM departments;
+
+
--- module_9_dates_relationships_join/practice_task.md
+++ module_9_dates_relationships_join/practice_task.md
@@ -3,3 +3,5 @@ practice_tasks:https://like-frog-b41.notion.site/SQL-Join-Practice-Task-27ac9794
 github:https://github.com/Apollo-Level2-Web-Dev/dbms-postgres
 
 
+Module 9: https://docs.google.com/document/d/1VOqXm0SM95hiY80o7c6mkHxOJoO1_wmKX7Iuf5jZ8BY/edit?tab=t.0#heading=h.rc4hmc4pbf95
+
